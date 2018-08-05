b96163fa06aebd23963a99f7a1d10773e5135493
hisanori <hisary@gmail.com>
2018-08-05 19:57:57 +0900

    save window size and position.

--- main.js
+++ main.js
@@ -2,10 +2,12 @@
 
 const {app, shell, BrowserWindow} = require('electron');
 
+const config = require('./lib/config');
 
 let win = null;
 
 app.on('ready', () => {
+    config.init();
     createWindow();
 });
 
@@ -24,15 +26,23 @@ function createWindow() {
         return;
     }
 
-    win = new BrowserWindow({ width: 480, height: 800 });
+    var bound = config.get('windowBounds', { width: 480, height: 800, x: 50, y: 60 });
+
+    win = new BrowserWindow(bound);
 
     win.loadURL('https://mobile.twitter.com/');
+
+    win.on('close', () => {
+        const bound = win.getBounds();
+        config.set('windowBounds', bound);
+        config.save();
+    });
 
     win.on('closed', () => {
         win = null;
     });
 
-    win.webContents.on("new-window", function(e, url) {
+    win.webContents.on('new-window', function(e, url) {
         e.preventDefault();
         shell.openExternal(url);
     });
