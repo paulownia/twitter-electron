028126c8a600e007da6d064ffe1a3d8d0fcf94ab
hisanori <hisary@gmail.com>
2018-10-18 23:29:05 +0900

    add menu

--- main.js
+++ main.js
@@ -1,49 +1,23 @@
 'use strict';
 
-const {app, shell, BrowserWindow} = require('electron');
+const { app } = require('electron');
 
 const config = require('./lib/config');
-
-let win = null;
+const menu = require('./lib/menu');
+const window = require('./lib/window');
+const event = require('./lib/event');
 
 app.on('ready', () => {
     config.init();
-    createWindow();
+    menu.init();
+    window.init();
+
 });
 
 app.on('window-all-closed', () => {
     app.quit();
 });
 
-/*
-app.on('activate', () => {
-    createWindow();
+event.on('select-quit', () => {
+    app.quit();
 });
-*/
-
-function createWindow() {
-    if (win !== null) {
-        return;
-    }
-
-    var bound = config.get('windowBounds', { width: 480, height: 800, x: 50, y: 60 });
-
-    win = new BrowserWindow(bound);
-
-    win.loadURL('https://mobile.twitter.com/');
-
-    win.on('close', () => {
-        const bound = win.getBounds();
-        config.set('windowBounds', bound);
-        config.save();
-    });
-
-    win.on('closed', () => {
-        win = null;
-    });
-
-    win.webContents.on('new-window', function(e, url) {
-        e.preventDefault();
-        shell.openExternal(url);
-    });
-}
