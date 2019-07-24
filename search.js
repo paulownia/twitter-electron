685f58b2caddf4061c892f726907db7e60c84a5b
hisanori <hisary@gmail.com>
2019-07-24 21:17:30 +0900

    fix coding style

--- search.js
+++ search.js
@@ -2,29 +2,29 @@ const { remote } = require('electron');
 const event = remote.require('./lib/event');
 
 document.addEventListener('DOMContentLoaded', () => {
-    const search = document.getElementById('search');
-    search.focus();
+  const search = document.getElementById('search');
+  search.focus();
 
-    search.addEventListener('keypress', (e) => {
-        if (e.keyCode !== 13) {  // enter
-            return;
-        }
+  search.addEventListener('keypress', (e) => {
+    if (e.keyCode !== 13) {  // enter
+      return;
+    }
 
-        e.preventDefault();  // prevent submit
+    e.preventDefault();  // prevent submit
 
-        var raw = search.value;
-        if (!raw) {
-            return;
-        }
+    let raw = search.value;
+    if (!raw) {
+      return;
+    }
 
-        const encoded = encodeURIComponent(raw);
+    const encoded = encodeURIComponent(raw);
 
-        event.emit('search',{ raw: raw, encoded: encoded });
-        event.emit('search-end');
-        search.value = '';
-    });
-    search.addEventListener('blur', () => {
-        event.emit('search-end');
-        search.value = '';
-    });
+    event.emit('search',{ raw: raw, encoded: encoded });
+    event.emit('search-end');
+    search.value = '';
+  });
+  search.addEventListener('blur', () => {
+    event.emit('search-end');
+    search.value = '';
+  });
 });
