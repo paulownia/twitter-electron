58b879890bbe7069614356542699aa90b1205a91
hisanori <hisary@gmail.com>
2021-04-07 14:28:49 +0900

    remove console.log

--- preference.js
+++ preference.js
@@ -3,13 +3,11 @@ document.addEventListener('DOMContentLoaded', async () => {
   const externalBrowser = document.getElementById('externalBrowser');
 
   externalBrowser.addEventListener('change', async (e) => {
-    console.log(externalBrowser.value);
     e.preventDefault();
     await api.setExternalBrowser(externalBrowser.value);
   });
 
   const defaultVal = await api.getExternalBrowser();
-  console.log(defaultVal);
 
   if (defaultVal) {
     externalBrowser.value = defaultVal;
