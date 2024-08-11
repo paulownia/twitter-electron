45b29ae40113ff2e4c7278c9fe7b67e9545ab664
hisanori <hisary@gmail.com>
2024-08-12 00:47:41 +0900

    update eslint
    remove an old eslint config
    add a new eslint config
    fix some rules

--- README.md
+++ README.md
@@ -9,3 +9,11 @@ check them.
 
 - https://releases.electronjs.org/
 - https://www.electronjs.org/docs/latest/tutorial/electron-timelines
+
+## ESlint
+
+using flat config.
+
+### vscode settings
+
+- Enable `Eslint: Use Flat Config` on this workspace
---
+++ eslint.config.js
@@ -0,0 +1,42 @@
+import pluginJs from '@eslint/js';
+import globals from 'globals';
+
+export default [
+  {
+    ignores: [
+      'dist/',
+      'hoge.js',
+      'fuga.js',
+      'piyo.js',
+    ],
+  },
+  pluginJs.configs.recommended,
+  {
+    languageOptions: {
+      globals: {
+        ...globals.browser,
+        ...globals.node,
+      },
+    },
+    rules: {
+      'semi': 2,
+      'no-console': 1,
+      'comma-dangle': [2, 'always-multiline'],
+      'quotes': [2, 'single'],
+      'no-irregular-whitespace': 2,
+      'no-spaced-func': 2,
+      'block-spacing': 2,
+      'indent': ['error', 2],
+      'no-unused-vars': ['error', {
+        'argsIgnorePattern': '^_',
+        'varsIgnorePattern': '^_',
+        'caughtErrorsIgnorePattern': '^_',
+      }],
+      'no-var': ['error'],
+      'no-constant-condition': ['error', { 'checkLoops': false }],
+      'space-before-blocks': 2,
+      'space-in-parens': 2,
+      'keyword-spacing': 2,
+    },
+  },
+];
