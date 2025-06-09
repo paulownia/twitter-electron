6aef4a2fbfe1a705a13709562844e5d563f3ee23
hisanori <hisary@gmail.com>
2025-06-09 23:31:37 +0900

    add no-sign option to build script

--- build.js
+++ build.js
@@ -3,6 +3,16 @@ import { packager } from '@electron/packager';
 import dotenv from 'dotenv';
 dotenv.config();
 
+// Handle command line arguments
+const allowedOption = '--no-sign';
+const args = process.argv.slice(2);
+const noSign = args.includes(allowedOption);
+
+if (args.length > 0 && (!noSign || args.length > 1)) {
+  console.error('Only the --no-sign option is supported.');
+  process.exit(1);
+}
+
 packager({
   out: 'build',
   dir: '.',
@@ -12,8 +22,8 @@ packager({
   platform: 'darwin',
   arch: 'arm64',
   extendInfo: './Info.plist',
-  osxSign: {},
-  osxNotarize: {
+  osxSign: noSign ? undefined : {},
+  osxNotarize: noSign ? undefined : {
     appleId: process.env.APPLE_ID,
     appleIdPassword: process.env.APPLE_ID_PASSWORD,
     teamId: process.env.APPLE_TEAM_ID,
